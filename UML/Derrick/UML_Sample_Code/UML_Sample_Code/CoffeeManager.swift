67b3849b22ad2699de079e0b6e8a7651761ad6d7
derrickkim0109 <derrickkim0109@naver.com>
2022-05-14 11:46:31 +0900

    docs : "UML" 프로젝트 완성

---
+++ UML/Derrick/UML_Sample_Code/UML_Sample_Code/CoffeeManager.swift
@@ -0,0 +1,14 @@
+//
+//  CoffeeManager.swift
+//  UML_Sample
+//
+//  Created by Derrick kim on 2022/05/14.
+//
+
+import Foundation
+
+class CoffeeManager {
+    func brew(_ menu: CoffeeMenu) {
+        print("\(menu)가 나왔습니다.")
+    }
+}
