be46353d7f69ba7e610adaf4123f65c015706f93
derrickkim0109 <derrickkim0109@naver.com>
2022-05-14 09:13:47 +0900

    feat : "UML" 디렉토리 생성

---
+++ UML/Derrick/UML_Sample/UML_Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UML_Sample
+//
+//  Created by Derrick kim on 2022/05/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
