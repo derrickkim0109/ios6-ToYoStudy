ac7d90589cedde150ce25c9fb857cac1504c4a2d
웡빙 <wb277@naver.com>
2022-05-28 15:00:11 +0900

    웡빙 토요스터디 고차함수 업로드

--- Higher-order-functions/웡빙/Higher-oreder-functionsPractice/main.swift
+++ Higher-order-functions/웡빙/Higher-oreder-functionsPractice/main.swift
@@ -5,10 +5,89 @@
 //  Created by 허건 on 2022/05/28.
 //
 
+
 import Foundation
 
-// Tp Do 1
+//MARK: - 실험 1
+struct Character {
+    var punch: () -> Void = { print("퍽퍽") }
+    var jump: () -> Void = { print("짬프") }
+    var sprint: () -> Void = { print("슈슉달리기") }
 
-// To Do 2
+    func tabShiftKey(skill: () -> Void) {
+        return skill()
+    }
+}
 
-// To Do 3
+let wongbing = Character()
+wongbing.tabShiftKey(skill: wongbing.jump)
+wongbing.tabShiftKey(skill: wongbing.punch)
+wongbing.tabShiftKey {
+    wongbing.sprint()
+}
+
+//MARK: - 실험 2
+
+var list = [1,2,3,"하나","둘","셋"] as [Any]
+
+var onlyStringList = list.filter { ($0 as? String) != nil }
+var onlyIntList = list.filter { ($0 as? Int) != nil } as [Int]
+
+var onlyStringList2 = list.compactMap { ($0 as? String)}
+var onlyIntList2 = list.compactMap { ($0 as? Int) }
+//
+//print(onlyStringList)
+//print(onlyIntList)
+
+var list2 = ["일","이","삼"]
+var noSpaceStr = list2.reduce("", +)
+print(noSpaceStr)
+//
+var list3 = [1, 2, 3]
+//var sumAllNumber = list3.reduce(0,-)
+//print(sumAllNumber)
+//
+//var expectation = ["일이삼", 123] as [Any]
+
+var conditionSumNumber = list3.filter { $0 > 1 }.reduce(0,+)
+print(conditionSumNumber)
+
+var list4 = ["하나","둘","셋"]
+var pastList = list4.map { "봉골레 파스타 " + $0 }
+print(pastList)
+
+
+//MARK: - 실험 3
+
+extension Sequence {
+    @inlinable // 자주 사용될, 호출이 자주 될 함수를 명시 해주어서
+    public func myMap<T>(_ transform: (Element) throws -> T) rethrows -> [T] {
+        let initialCapacity = underestimatedCount
+        var result = ContiguousArray<T>()
+        result.reserveCapacity(initialCapacity) // 초기 수용 범위 설정
+        
+        var iterator = self.makeIterator()
+        
+        for _ in 0..<initialCapacity {
+            result.append(try transform(iterator.next()!))
+        }
+        while let element = iterator.next() {
+            result.append(try transform(element))
+        }
+        return Array(result)
+    }
+}
+
+var list = [1,2,3]
+print(list.myMap {
+    $0 + 1
+})
+
+func myReduce(){}
+func myFilter(){}
+
+list.map { throw myError.error1 }
+
+enum myError: Error {
+    case error1
+}
