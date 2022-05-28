32b90256faba014a7c521c9366daa52d761e6ea0
derrickkim0109 <derrickkim0109@naver.com>
2022-05-29 00:00:02 +0900

    feat : 고차함수

--- Higher-order-functions/Derrick/Higher-oreder-functionsPractice/main.swift
+++ Higher-order-functions/Derrick/Higher-oreder-functionsPractice/main.swift
@@ -7,8 +7,54 @@
 
 import Foundation
 
-// Tp Do 1
+//MARK: - 실험 1
+class Character {
+    func tabShiftKey(skill: () -> Void) {
+        return skill()
+    }
+}
 
-// To Do 2
+class Skills {
+    let hit: () -> Void = {
+        print("때리기")
+    }
+    
+    let gain: (_ name: String) -> Void = { name in
+        guard !name.elementsEqual("derrick") else {
+            print("\(name)이 수금 중")
+            return
+        }
+        print("\(name)의 돈을 얻다")
+    }
+}
 
-// To Do 3
+//MARK: - 실험 2
+let skills = Skills()
+let students: [String] = ["derrick", "brad", "baekgom", "wongbing"]
+let character = Character()
+
+students.map {_ in
+    print(character.tabShiftKey(skill: skills.hit))
+}
+
+//MARK: - 실험 3
+extension Sequence {
+    // 자주 사용될, 호출이 자주 될 함수를 명시 해주어서
+    @inlinable
+    public func myMap<T>(_ transform: (Element) throws -> T) rethrows -> [T] {
+        let initialCapacity = underestimatedCount
+        var result = ContiguousArray<T>()
+        // 초기 수용 범위 설정
+        result.reserveCapacity(initialCapacity)
+        
+        var iterator = self.makeIterator()
+        
+        for _ in 0..<initialCapacity {
+            result.append(try transform(iterator.next()!))
+        }
+        while let element = iterator.next() {
+            result.append(try transform(element))
+        }
+        return Array(result)
+    }
+}
